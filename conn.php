ee1d824cac52d85122aa9f22faba9b2c8dd2602a
hieubossgr <hieubossgr@gmail.com>
2022-10-17 16:31:45 +0700

    Confix file connect database

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","0562324","pos");
+$conn = mysqli_connect("localhost","hieu","Hieu1234!","pos");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
