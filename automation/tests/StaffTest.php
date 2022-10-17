9e49a6c608641267a5d13684a61fe03b1eb37696
hieubossgr <hieubossgr@gmail.com>
2022-10-18 00:32:39 +0700

    test

--- automation/tests/StaffTest.php
+++ automation/tests/StaffTest.php
@@ -27,7 +27,7 @@ class StaffTest extends TestCase
     public function only_first_name()
     {
         $staff = new Staff;
-        $staff->first_name = "AA";
+        $staff->first_name = "AiA";
         $this->assertEquals('AAA', $staff->getFullName());
     }
 }
