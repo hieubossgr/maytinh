8eb32c4f27bc05dbb786829cc46f1656272b9615
hieubossgr <hieubossgr@gmail.com>
2022-10-25 17:20:26 +0700

    modify

--- header.php
+++ header.php
@@ -9,11 +9,11 @@
 
     <title>PHÂN PHỐI HÀNG HÓA</title>
 	
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
-    <link href="vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="vendo/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendo/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="vendo/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+    <link href="vendo/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="vendo/metisMenu/metisMenu.min.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">	
     <link href="dist/css/sb-admin-2.css" rel="stylesheet">
 	
